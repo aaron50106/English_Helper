5fac7caff222193e4f6f73b5e6b6f9e0c9dc1502
aaron50106 <130100268+aaron50106@users.noreply.github.com>
2026-09-18 00:12:01 +0800

    Update app.py

--- English_Helper/English_Helper_Web/app.py
+++ English_Helper/English_Helper_Web/app.py
@@ -412,4 +412,4 @@ def records():
 
 if __name__ == "__main__":
     LOGGER.info("Starting English Helper web server on http://127.0.0.1:5000")
-    app.run(host="127.0.0.1", port=5000, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
